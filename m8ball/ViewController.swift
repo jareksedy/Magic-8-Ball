56dd972a36af23c03dadf6185369c0b37096c595
Jarek <jareksedy@icloud.com>
2021-06-23 00:28:14 +0600

    added basic ball shape.

--- m8ball/ViewController.swift
+++ m8ball/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    func delay(_ delay:Double, closure:@escaping ()->()) {
+        let when = DispatchTime.now() + delay
+        DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+    }
+    
+    @IBOutlet weak var bgView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let ballOuterSize: CGFloat = 500
+        let ballInnerSize: CGFloat = 275
+        
+        let ballOuter = UIView()
+        let ballInner = UIView()
+        
+        ballOuter.backgroundColor = UIColor.black
+        ballInner.backgroundColor = UIColor.white
+        
+        ballOuter.frame = CGRect(x: 0, y: 0, width: ballOuterSize, height: ballOuterSize)
+        ballInner.frame = CGRect(x: 0, y: 0, width: ballInnerSize, height: ballInnerSize)
+        
+        ballOuter.center = CGPoint(x: bgView.bounds.midX, y: bgView.bounds.maxY - ballOuterSize / 4)
+        ballInner.center = CGPoint(x: ballOuter.bounds.midX, y: ballOuter.bounds.minY + ballInnerSize / 2 - 10)
+        
+        ballOuter.layer.cornerRadius = ballOuterSize / 2
+        ballInner.layer.cornerRadius = ballInnerSize / 2
 
-
+        ballOuter.layer.masksToBounds = true
+        
+        ballInner.transform = CGAffineTransform(scaleX: 1, y: 0.85)
+        
+        bgView.addSubview(ballOuter)
+        ballOuter.addSubview(ballInner)
+        
+        
+    }
 }
 
